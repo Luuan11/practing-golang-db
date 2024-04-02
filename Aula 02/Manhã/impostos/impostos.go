e9f1d1f9417023ba8f878d7d70f5b69c8b7eb773
luafernando_meli <luan.fschaves@mercado.livre>
2024-04-02 10:00:21 -0300

    :sparkles: feat: ex manhã

---
+++ Aula 02/Manhã/impostos/impostos.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	
+	fmt.Printf("imposto de até R$50.000: %.2f", calcularImposto(50000))
+	fmt.Printf("imposto de até R$150.000: %.2f", calcularImposto(150000))
+	fmt.Printf("imposto mais que R$150.000: %.2f", calcularImposto(150001))
+}
+
+func calcularImposto(salary float64) float64{
+	if salary <= 50000 {
+		return 0.0
+	} else if salary <= 150000.00 {
+		return salary * 0.17
+	} 
+	return salary * 0.27
+}
