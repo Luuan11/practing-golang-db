218d4e40d70c1352bcb7e18147a866cd6efa63e1
luafernando_meli <luan.fschaves@mercado.livre>
2024-04-03 18:38:06 -0300

    feat: exe aula03 manha

--- Aula 03/Manhã/guardar/main.go
+++ Aula 03/Manhã/guardar/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"os"
+)
+
+type produto struct{
+	id 		int
+	qtd 	int
+	preco 	float64
+}
+
+func guardarArquivo(produto []produto, fileName string) {
+	var valores []byte
+	valores = append(valores, "ID,PRECO,QUANTIDADE\n"...)
+
+	for _, produtoItem := range produto{
+		valores = append(valores, fmt.Sprintf(" %d , %2.f , %d\n" , produtoItem.id, produtoItem.preco, produtoItem.qtd)...)
+	}
+
+	err := os.WriteFile(fileName+".csv", valores, 0644)
+	if err != nil{
+		fmt.Println("Erro")
+		return
+	}
+}
+
+func main() {
+	produtos := []produto{
+		{
+			id:         1,
+			qtd: 8,
+			preco:      6.99,
+		},
+		{
+			id:         2,
+			qtd: 		20,
+			preco:      12.99,
+		},
+		{
+			id:         3,
+			qtd: 		10,
+			preco:      9.99,
+		},
+	}
+
+	guardarArquivo(produtos, "produtos")
 }
--- Aula 03/Manhã/ler/main.go
+++ Aula 03/Manhã/ler/main.go
@@ -1,5 +1,65 @@
 package main
 
-func main(){
-	
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type produto struct{
+	id 		int
+	qtd 	int
+	preco 	float64
+}
+
+func produtoLista(data []string) ([]produto,error) {
+	produtos := make([]produto, 0, len(data))
+	for i, row := range data{
+		if i == 0{
+			continue
+		}
+
+		var p produto
+		fmt.Sscanf(row, "%d,%f,%d", &p.id, &p.preco, &p.qtd)
+		produtos = append(produtos, p)
+	}
+	return produtos, nil
+}
+
+func lerArquivo(filename string)([]produto, error){
+	arq, err := os.Open(filename)
+	if err != nil{
+		return nil, errors.New("Erro ao tentar abrir o arquivo informado")
+	}
+
+	defer arq.Close()
+
+	scanner := bufio.NewScanner(arq)
+	var data []string
+	for scanner.Scan(){
+		data = append(data, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, errors.New("erro ao tentar ler o arquivo informado")
+	}
+
+	return produtoLista(data)
+}
+
+func main() {
+	produtos, err := lerArquivo("../guardar/produtos.csv")
+	if err != nil {
+		fmt.Println("Erro")
+		return
+	}
+
+	fmt.Printf("ID\tPreco\tQuantidade\n")
+	var total float64 = 0.0
+	for _, p := range produtos {
+		fmt.Printf("%d\t%.2f\t%d\n", p.id, p.preco, p.qtd)
+		total += p.preco * float64(p.qtd)
+	}
+	fmt.Printf("\t%.2f\n", total)
 }
