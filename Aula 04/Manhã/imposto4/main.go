e1c821cf0c07561d178a2deb71cc2ad72a648be0
luafernando_meli <luan.fschaves@mercado.livre>
2024-04-05 14:48:34 -0300

    feat: exe aula 04 manha

--- Aula 04/Manhã/imposto4/main.go
+++ Aula 04/Manhã/imposto4/main.go
@@ -1,7 +1,45 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
 
+type empresa struct{
+	funcionario 	    string
+	horasTrabalhadas	int
+	valorHora			int
+}
+
+func (e empresa) salarioMensal()(float64, error){
+	salario := float64(e.horasTrabalhadas) * float64(e.valorHora)
+	if salario >= 20000 {
+		salarioImposto := salario * 0.10
+		salario -= salarioImposto
+		fmt.Printf("Seu salario foi de %.2f e terá que pagar imposto \n", salario)
+	} else{
+		fmt.Printf("Seu salario foi de %.2f e não precisar pagar imposto \n", salario)
+	}
+	
+	if e.horasTrabalhadas < 80 && e.horasTrabalhadas > 0 {
+		return 0, errors.New("erro: o trabalhador não pode ter trabalhado menos de 80 horas por mês")
+	}
+
+	return salario, nil
+}
 
 func main() {
-	
+	funcionario1 := empresa{
+		funcionario: 	"Lucas",
+		horasTrabalhadas: 90,
+		valorHora: 200.0,
+	}
+
+	salario, err := funcionario1.salarioMensal()
+	if err != nil{
+		fmt.Println("Erro", err)
+	} else {
+		fmt.Printf("O salario do funcionário %s é de: %.2f Mensais \n", funcionario1.funcionario, salario)
+	}
+
 }
