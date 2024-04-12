937f2703f812aa37a91a6c3a5ea4e7ee81838057
luafernando_meli <luan.fschaves@mercado.livre>
2024-04-12 18:40:06 -0300

    :sparkles: feat: exe aula 03 tarde

--- GO WEB/Aula 03/godotenvATV/cmd/server/handler/users.go
+++ GO WEB/Aula 03/godotenvATV/cmd/server/handler/users.go
@@ -1,14 +1,25 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/luuan11/godotENV/internal/users"
 )
 
 //implementando request
-type request struct{
+// type request struct{
+// 	Name 			string   	`json:"name"`
+// 	Sobrenome		string		`json:"lastname"`
+// 	Email			string		`json:"email"`
+// 	Age 			uint64	    `json:"age"`
+// 	Status 			bool	  	`json:"status"`
+// 	DateCreation 	string 		`json:"dateCreation"`
+// }
+
+type CreateRequestDto struct {
 	Name 			string   	`json:"name"`
 	Sobrenome		string		`json:"lastname"`
 	Email			string		`json:"email"`
@@ -16,52 +27,52 @@ type request struct{
 	Status 			bool	  	`json:"status"`
 	DateCreation 	string 		`json:"dateCreation"`
 }
-//estrutura controller
-type User struct{
+
+type UpdateRequestDto struct {
+	Name 			string   	`json:"name"`
+	Sobrenome		string		`json:"lastname"`
+	Email			string		`json:"email"`
+	Age 			uint64	    `json:"age"`
+	Status 			bool	  	`json:"status"`
+	DateCreation 	string 		`json:"dateCreation"`
+}
+
+type UpdateNameRequestDto struct {
+	Name string `json:"name"`
+}
+
+type UserHandler struct {
 	service users.Service
 }
 
-func NewUser(u users.Service) *User{
-	return &User{
-		service: u,
+func NewUser(p users.Service) *UserHandler {
+	return &UserHandler{
+		service: p,
 	}
 }
 
-func (c *User)GetAll() gin.HandlerFunc{
-	return func(ctx *gin.Context){
-		token := ctx.Request.Header.Get("token")
-			if token != "123456"{
-				ctx.JSON(http.StatusUnauthorized, gin.H{
-					"Error": "Token Invalido",
-				})
-				return
-			}
-			
-			u, err := c.service.GetAll()
-			if err != nil{
-				ctx.JSON(http.StatusNotFound, gin.H{
-					"error" : err.Error(),
-				})
-				return
-			}
-
-			if len(u) == 0{
-				ctx.Status(http.StatusNoContent)
-			} 
-
-			ctx.JSON(http.StatusOK, u)
-
-		}
-}
-
-func (c *User) Store() gin.HandlerFunc{
+func (c *UserHandler) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		token := ctx.GetHeader("token")
-		if token != "123456" {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "token invalido"})
+		p, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
-		var req request
+
+		if len(p) == 0 {
+			ctx.Status(http.StatusNoContent)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, p)
+	}
+}
+
+func (c *UserHandler) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req CreateRequestDto
 		if err := ctx.Bind(&req); err != nil {
 			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
 				"error": err.Error(),
@@ -69,13 +80,114 @@ func (c *User) Store() gin.HandlerFunc{
 			return
 		}
 
-		// fmt.Println(req.Name, req.Sobrenome, req.Email, req.Age, req.Status, req.Status, req.DateCreation)
-		u, err := c.service.Store(req.Name, req.Sobrenome, req.Email, req.Age, req.Status, req.DateCreation)
+		// quando chamamos a service, os dados já estarão tratados
+		fmt.Println(req.Name, req.Sobrenome, req.Email, req.Age,req.Status, req.DateCreation)
+		p, err := c.service.Store(req.Name, req.Sobrenome, req.Email, req.Age, req.Status, req.DateCreation)
 		if err != nil {
-			ctx.JSON(http.StatusNotFound, gin.H{
-				"error": err.Error()})
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		ctx.JSON(http.StatusCreated, u)	
+		ctx.JSON(http.StatusCreated, p)
+	}
+}
+
+func (c *UserHandler) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		id, err := strconv.ParseUint(ctx.Param("userId"), 10, 0)
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid ID"})
+			return
+		}
+
+		var req UpdateRequestDto
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			return
+		}
+
+		fmt.Println(req.Name, req.Sobrenome, req.Email, req.Age,req.Status, req.DateCreation)
+
+		if req.Name == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "o nome do user é obrigatório"})
+			return
+		}
+
+		if req.Sobrenome == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "o sobrenome do user é obrigatório"})
+			return
+		}
+
+		if req.Email == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "o email do user é obrigatória"})
+			return
+		}
+
+		if req.Age > 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "a idade do user tem que ser maior de 6 anos"})
+			return
+		}
+
+		if req.Age > 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "a idade do user tem que ser maior de 6 anos"})
+			return
+		}
+
+		if req.DateCreation == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "a data de crição precisa está preenchida"})
+			return
+		}
+
+		p, err := c.service.Update(id, req.Name, req.Sobrenome, req.Email, req.Age,req.Status, req.DateCreation)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, p)
+	}
+}
+
+func (c *UserHandler) UpdateName() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		id, err := strconv.ParseUint(ctx.Param("userId"), 10, 0)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid ID"})
+			return
+		}
+		var req UpdateNameRequestDto
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			return
+		}
+		if req.Name == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "O nome do user é obrigatório"})
+			return
+		}
+		p, err := c.service.UpdateName(id, req.Name)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, p)
+	}
+}
+
+func (c *UserHandler) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		id, err := strconv.ParseUint(ctx.Param("userId"), 10, 0)
+		if err != nil {
+			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			return
+		}
+		err = c.service.Delete(id)
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"data": fmt.Sprintf("O user %d foi removido", id)})
 	}
 }
--- GO WEB/Aula 03/godotenvATV/cmd/server/main.go
+++ GO WEB/Aula 03/godotenvATV/cmd/server/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"log"
+	"os"
+	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/luuan11/godotENV/cmd/server/handler"
 	"github.com/luuan11/godotENV/internal/users"
+	"github.com/luuan11/godotENV/pkg/store"
 )
 
+func LoggerMiddleware(ctx *gin.Context) {
+	fmt.Printf("[%s] %s\n", ctx.Request.Method, ctx.Request.URL)
+	ctx.Next()
+}
+
+func TokenMiddleware(ctx *gin.Context) {
+	tokenEnvironment := os.Getenv("TOKEN")
+	token := ctx.GetHeader("token")
+	if token != tokenEnvironment {
+		// status StatusUnauthorized equivalente ao 401
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"error": "token inválido",
+		})
+		return
+	}
+	ctx.Next()
+}
+
 func main(){
-	_ = godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file", err)
+	}
 
+	db := storeUser.NewFileStore("file", "users.json")
 
-	repo := users.NewRepository()
+	user := os.Getenv("MY_USER")
+	password := os.Getenv("MY_PASS")
+
+	fmt.Println("user", user, "pass", password)
+
+	repo := users.NewRepository(db)
 	service := users.NewService(repo)
 	userHandler := handler.NewUser(service)
 
@@ -21,6 +52,12 @@ func main(){
 	u := server.Group("/users")
 	u.POST("/", userHandler.Store())
 	u.GET("/", userHandler.GetAll())
+	server.Use(LoggerMiddleware)
+
+	u.Use(TokenMiddleware)
+	u.PUT("/:productId", userHandler.Update())
+	u.PATCH("/:productId", userHandler.UpdateName())
+	u.DELETE("/:productId", userHandler.Delete())
 
 	server.Run()
 
